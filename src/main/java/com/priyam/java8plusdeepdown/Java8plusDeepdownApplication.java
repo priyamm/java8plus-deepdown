484570ce3a4cf1bac3256cd702598b08d6205baf
Priyam Gupta <priyam.gupta@birdeye.com>
2021-03-28 20:11:17 +0530

    first-commit

---
+++ src/main/java/com/priyam/java8plusdeepdown/Java8plusDeepdownApplication.java
@@ -0,0 +1,13 @@
+package com.priyam.java8plusdeepdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8plusDeepdownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8plusDeepdownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/priyam/java8plusdeepdown/Java8plusDeepdownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyam.java8plusdeepdown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8plusDeepdownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
